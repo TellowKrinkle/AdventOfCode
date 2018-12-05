04e6e97b3b3317a6426ec51e7bf329806075fc84
tellowkrinkle <tellowkrinkle@users.noreply.github.com>
2018-12-04 23:41:42 -0600

    Add 2018 Day 5 Solution

---
+++ 2018/2018Day5.swift
@@ -0,0 +1,37 @@
+func aocD4a(_ inputStr: String) {
+	var chars = ""
+	for char in inputStr {
+		guard let prev = chars.last else { chars.append(char); continue }
+		if String(prev).lowercased() == String(char).lowercased() && prev != char {
+			_ = chars.popLast()
+		}
+		else {
+			chars.append(char)
+		}
+	}
+	print(chars.count)
+}
+
+func aocD4b(_ inputStr: String) {
+	let lengths = Set(inputStr.lazy.map { String($0).lowercased() }).map { letter -> Int in
+		var chars = ""
+		for char in inputStr {
+			guard String(char).lowercased() != letter else { continue }
+			guard let prev = chars.last else { chars.append(char); continue }
+			if String(prev).lowercased() == String(char).lowercased() && prev != char {
+				_ = chars.popLast()
+			}
+			else {
+				chars.append(char)
+			}
+		}
+		return chars.count
+	}
+	print(lengths.min()!)
+}
+
+import Foundation
+let str = try! String(contentsOfFile: CommandLine.arguments[1])
+
+aocD4a(str)
+aocD4b(str)
